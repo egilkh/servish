bef0b13533f35b13c9318d47f8d44af2fc2f094b
Egil Hanger <egilkh@gmail.com>
2011-04-26 05:36:28 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Servish
+
+A simple webserver for serving a single folder.
+Written for NodeJS to help with my web development.
+
+Not in a working order.
---
+++ servish.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+/**
+ * A slightly simple webserver for serving a folder
+ */
+// requires
+var http = require('http');
+var util = require('util');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
+var defaults = {
+	ip: null, // listen to all ip's
+
+	// first and last port to try out when starting to listen
+	// give up if last < first or if none of the ports are free to use
+	port: {
+		first: 8080,
+		last: 8100
+	},
+	// default mime types
+	mime: {
+		types: {
+			'.txt': 'text/plain',
+			'.js': 'text/javascript',
+			'.css': 'text/css',
+			'.html': 'text/html',
+			'.png': 'image/png',
+			'.jpg': 'image/jpg'
+		}
+	}
+}
+
+// basepath for this one
+var docRoot = process.cwd();
+
+// functions
+var request_cb = function (req, res) {
+
+	res.setHeader('Content-Type', defaults.mime.types['.txt']); // default mime!
+	res.setHeader('Content-Encoding', 'utf-8');
+	res.setHeader('Connection', 'close');
+
+	var requestUrl = url.parse(req.url);
+	var requestedDocument = path.join(docRoot, requestUrl.pathname);
+
+	util.log('Connection from client: ' + requestUrl.pathname  + ' (' + req.socket.remoteAddress + ').');
+
+	path.exists(requestedDocument, function(exists) {
+		if (!exists) {
+			var msg = '404 - File not found.';
+			res.setHeader('Content-Length', msg.length);
+			res.writeHead(404);
+			res.end(msg);
+		} else {
+			fs.stat(requestedDocument, function(err, stats) {
+				if (err) { throw err; }
+				if (stats.isFile()) {
+					var ext = path.extname(path.basename(requestedDocument));
+					var mime = defaults.mime.types[ext] ? defaults.mime.types[ext] : defaults.mime.types['.txt'];
+					res.setHeader('Content-Type', mime);
+					res.setHeader('Content-Length', stats.size);
+					res.writeHead(200);
+					fs.readFile(requestedDocument, 'utf8', function(err, data) {
+						if (err) { throw err; }
+						res.write(data, 'utf8');
+						res.end();
+					});
+				} else if (stats.isDirectory()) {
+					// TODO pretty print directory
+					var msg = '200 - Directory Listing.';
+					res.setHeader('Content-Length', msg.length);
+					res.writeHead(200);
+					res.end(msg);
+				} else {
+					// this means we no want to serve them people
+					var msg = '500 - I no know what do!';
+					res.setHeader('Content-Length', msg.length);
+					res.writeHead(500);
+					res.end(msg);
+				}
+			});
+		}
+	});
+};
+
+var tryListen = function (serv, port) {
+	util.log('Servish, trying to bind: ' + port);
+	// TODO
+	// this triggers twice, but I'm not sure which event this is
+	// so I can't remove it from the listeners.
+	serv.listen(port, defaults.ip, serverBound);
+};
+
+var serverBound = function () {
+	// we could probably do fancy tricks over here
+}
+
+// TODO
+// here we should read .servish from $HOME and extend our defaults
+
+
+util.log('Servish, serving: ' + docRoot);
+
+if (defaults.port.last < defaults.port.first) {
+	util.log('Servish, you might have an error with your config (PORT).');
+	process.exit(1);
+}
+
+var server = http.createServer(request_cb);
+var currentPort = defaults.port.first;
+server.on('error', function (e) {
+	if (e.code == "EADDRINUSE") { // probably a port problem
+		util.log('Servish, port in use. Retrying in 1s.');
+		setTimeout(function() {
+			// TODO Logic for port boundries
+			tryListen(server, ++currentPort);
+		}, 1000);
+	}
+});
+tryListen(server, currentPort);
+
+util.log('Servish, up and running (probably)!');
