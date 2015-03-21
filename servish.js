14431570a7a81a6e88f0d74b4b7df85bf8a70806
egilkh <egilkh@gmail.com>
2015-03-21 21:24:58 +0100

    Should be :retab for spaces.

--- servish.js
+++ servish.js
@@ -11,38 +11,38 @@ var fs = require('fs');
 
 var defaults = {
 
-	// ip to listen to, null means all ip's
-	ip: null, // listen to all ip's
+  // ip to listen to, null means all ip's
+  ip: null, // listen to all ip's
 
-	// first and last port to try out when starting to listen
-	// give up if last < first or if none of the ports are free to use
-	port: {
-		first: 8080,
-		last: 8100
-	},
+  // first and last port to try out when starting to listen
+  // give up if last < first or if none of the ports are free to use
+  port: {
+    first: 8080,
+    last: 8100
+  },
 
-	// should we show hidden files/folders (. files)
-	showHidden: false,
+  // should we show hidden files/folders (. files)
+  showHidden: false,
 
-	// default mime types, what I plan to serve ze best
-	mime: {
-		types: {
-			'.txt': 'text/plain; charset=utf-8',
-			'.js': 'text/javascript; charset=utf-8',
-			'.css': 'text/css; charset=utf-8',
-			'.html': 'text/html; charset=utf-8',
-			'.png': 'image/png; charset=utf-8',
-			'.jpg': 'image/jpg; charset=utf-8'
-		},
+  // default mime types, what I plan to serve ze best
+  mime: {
+    types: {
+      '.txt': 'text/plain; charset=utf-8',
+      '.js': 'text/javascript; charset=utf-8',
+      '.css': 'text/css; charset=utf-8',
+      '.html': 'text/html; charset=utf-8',
+      '.png': 'image/png; charset=utf-8',
+      '.jpg': 'image/jpg; charset=utf-8'
+    },
     binary: ['.jpg', '.png']
-	},
+  },
 
-	// a quite simple HTML5 template
-	template:
+  // a quite simple HTML5 template
+  template:
 '<!DOCTYPE html>\n\
 <html>\n\
 <head>\n\
-	<title>{title}</title>\n\
+  <title>{title}</title>\n\
 </head>\n\
 <body>\n\n\
 <h1>{title}</h1>\n\
@@ -53,18 +53,18 @@ var defaults = {
 
 // basic content for the various templates
 var pageContent = {
-	notFound: {
-		title: '404 - File not found',
-		content: 'This is not the file you are looking for.'
-	},
-	error: {
-		title: '500 - Error',
-		content: 'Do not know what to do.'
-	},
-	directoryListing : {
-		title: 'Directory Listing',
-		content: 'This should never be shown.'
-	}
+  notFound: {
+    title: '404 - File not found',
+    content: 'This is not the file you are looking for.'
+  },
+  error: {
+    title: '500 - Error',
+    content: 'Do not know what to do.'
+  },
+  directoryListing : {
+    title: 'Directory Listing',
+    content: 'This should never be shown.'
+  }
 };
 
 // basepath for this one
@@ -72,112 +72,112 @@ var documentRoot = process.cwd();
 
 // functions
 var fillTemplate = function (template, values) {
-	var t = template;
-	for (v in values) {
-		t = swapRecursive(t, '{' + v + '}', values[v]);
-	}
-	return t;
+  var t = template;
+  for (v in values) {
+    t = swapRecursive(t, '{' + v + '}', values[v]);
+  }
+  return t;
 };
 
 var swapRecursive = function (t, from, to) {
-	t = t.replace(from, to);
-	if (t.indexOf(from) > -1) {
-		t = swapRecursive(t, from, to);
-	}
-	return t;
+  t = t.replace(from, to);
+  if (t.indexOf(from) > -1) {
+    t = swapRecursive(t, from, to);
+  }
+  return t;
 };
 
 var requestCallback = function (req, res) {
 
-	res.setHeader('Connection', 'close'); // we no accept long calls
+  res.setHeader('Connection', 'close'); // we no accept long calls
 
-	var requestUrl = url.parse(req.url);
-	var requestedDocument = path.join(documentRoot, requestUrl.pathname);
+  var requestUrl = url.parse(req.url);
+  var requestedDocument = path.join(documentRoot, requestUrl.pathname);
 
-	var remoteAddress = req.socket.remoteAddress;
+  var remoteAddress = req.socket.remoteAddress;
 
-	util.log('Connection from client: ' + requestUrl.pathname  + ' (' + remoteAddress + ').');
+  util.log('Connection from client: ' + requestUrl.pathname  + ' (' + remoteAddress + ').');
 
-	fs.exists(requestedDocument, function(exists) {
-		if (!exists) {
-			var output = fillTemplate(defaults.template, pageContent.notFound);
-			//res.setHeader('Content-Length', output.length);
-			//res.setHeader('Content-Type', defaults.mime.types['.html']);
-			res.writeHead(404, {
-				'Content-Length': output.length,
-				'Content-Type': defaults.mime.types['.html']
-			});
-			res.end(output);
-			util.log('Served: 404 (' + remoteAddress + ').');
-		} else {
-			fs.stat(requestedDocument, function(err, stats) {
-				if (err) { throw err; }
-				if (stats.isFile()) {
+  fs.exists(requestedDocument, function(exists) {
+    if (!exists) {
+      var output = fillTemplate(defaults.template, pageContent.notFound);
+      //res.setHeader('Content-Length', output.length);
+      //res.setHeader('Content-Type', defaults.mime.types['.html']);
+      res.writeHead(404, {
+        'Content-Length': output.length,
+        'Content-Type': defaults.mime.types['.html']
+      });
+      res.end(output);
+      util.log('Served: 404 (' + remoteAddress + ').');
+    } else {
+      fs.stat(requestedDocument, function(err, stats) {
+        if (err) { throw err; }
+        if (stats.isFile()) {
 
-					var ext = path.extname(path.basename(requestedDocument));
-					var mime = defaults.mime.types[ext] ? defaults.mime.types[ext] : defaults.mime.types['.txt'];
+          var ext = path.extname(path.basename(requestedDocument));
+          var mime = defaults.mime.types[ext] ? defaults.mime.types[ext] : defaults.mime.types['.txt'];
 
-					res.writeHead(200, {
-						'Content-Type': mime,
-						'Content-Length': stats.size
-					});
+          res.writeHead(200, {
+            'Content-Type': mime,
+            'Content-Length': stats.size
+          });
 
           var readOptions = {};
           if (defaults.mime.binary.indexOf(ext) === -1) {
             readOptions.encoding = 'utf8';
           }
-					// create stream and pipe it, closes res when done
-					// TODO Fix sending of binary content ?
-					fs.createReadStream(requestedDocument, readOptions)
+          // create stream and pipe it, closes res when done
+          // TODO Fix sending of binary content ?
+          fs.createReadStream(requestedDocument, readOptions)
           .on('end', function () {
-						util.log('Served: ' + this.path + '(' + remoteAddress + ').');
-					}).on('error', function(ex){
-						// TODO
-						util.log('Error reading stream, ex: ' + ex);
-					}).pipe(res);
+            util.log('Served: ' + this.path + '(' + remoteAddress + ').');
+          }).on('error', function(ex){
+            // TODO
+            util.log('Error reading stream, ex: ' + ex);
+          }).pipe(res);
 
-				} else if (stats.isDirectory()) {
-					fs.readdir(requestedDocument, function(err, files) {
-						if (err) { throw err; }
+        } else if (stats.isDirectory()) {
+          fs.readdir(requestedDocument, function(err, files) {
+            if (err) { throw err; }
 
-						var page = {
-							title:requestUrl.pathname,
-							content:""
-						};
+            var page = {
+              title:requestUrl.pathname,
+              content:""
+            };
 
-						page.content = '<ul>\n';
-						for (f in files) {
-							if (files[f].substr(0, 1) == "." && !defaults.showHidden) {
-								continue;
-							}
+            page.content = '<ul>\n';
+            for (f in files) {
+              if (files[f].substr(0, 1) == "." && !defaults.showHidden) {
+                continue;
+              }
 
-							page.content += '\t<li><a href="' + files[f] + '">' + files[f] + '</a></li>\n';
-						}
-						page.content += '</ul>\n';
+              page.content += '\t<li><a href="' + files[f] + '">' + files[f] + '</a></li>\n';
+            }
+            page.content += '</ul>\n';
 
-						var output = fillTemplate(defaults.template, page);
-						res.writeHead(200, {
-							'Content-Type': defaults.mime.types['.html'],
-							'Content-Length': output.length
-						});
-						res.end(output);
-						util.log('Served: Directory Listing (' + remoteAddress + ').');
-					});
-				} else {
-					var output = fillTemplate(defaults.template, pageContent.error);
-					res.writeHead(500, {
-						'Content-Length': output.length
-					});
-					res.end(output);
-					util.log('We nowt sure what to do (' + remoteAddress + ').');
-				}
-			});
-		}
-	});
+            var output = fillTemplate(defaults.template, page);
+            res.writeHead(200, {
+              'Content-Type': defaults.mime.types['.html'],
+              'Content-Length': output.length
+            });
+            res.end(output);
+            util.log('Served: Directory Listing (' + remoteAddress + ').');
+          });
+        } else {
+          var output = fillTemplate(defaults.template, pageContent.error);
+          res.writeHead(500, {
+            'Content-Length': output.length
+          });
+          res.end(output);
+          util.log('We nowt sure what to do (' + remoteAddress + ').');
+        }
+      });
+    }
+  });
 };
 
 var serverBound = function (server) {
-	util.log('Servish, bound to ' + server.address().address + ':' + server.address().port + '.');
+  util.log('Servish, bound to ' + server.address().address + ':' + server.address().port + '.');
 };
 
 // TODO
@@ -186,31 +186,31 @@ var serverBound = function (server) {
 util.log('Servish, serving: ' + documentRoot);
 
 if (defaults.port.last < defaults.port.first) {
-	util.log('Servish, you might have an error with your config (PORT).');
-	process.exit(1);
+  util.log('Servish, you might have an error with your config (PORT).');
+  process.exit(1);
 }
 
 var server = http.createServer(requestCallback);
 var currentPort = defaults.port.first;
 server.on('error', function (e) {
-	if (e.code == "EADDRINUSE") { // probably a port problem
-		if ((currentPort + 1) == defaults.port.last) {
-			util.log('No port in range to bind to, exiting.');
-			process.exit(1);
-		}
-		util.log('Servish, port in use. Retrying in 1s.');
-		setTimeout(function() {
-			server.removeAllListeners('listening'); // prevent double listening event
-			server.listen(++currentPort, defaults.ip, function() {
-				serverBound(this);
-			});
-		}, 1000);
-	}
+  if (e.code == "EADDRINUSE") { // probably a port problem
+    if ((currentPort + 1) == defaults.port.last) {
+      util.log('No port in range to bind to, exiting.');
+      process.exit(1);
+    }
+    util.log('Servish, port in use. Retrying in 1s.');
+    setTimeout(function() {
+      server.removeAllListeners('listening'); // prevent double listening event
+      server.listen(++currentPort, defaults.ip, function() {
+        serverBound(this);
+      });
+    }, 1000);
+  }
 }).listen(currentPort, defaults.ip, function() {
-	serverBound(this);
+  serverBound(this);
 });;
 
 process.on('SIGINT', function () {
-	util.log('Got SIGINT, shuting down.');
-	server.close();
+  util.log('Got SIGINT, shuting down.');
+  server.close();
 });
