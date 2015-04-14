46959ffd79ac00ae11af6a95137dc23d450b68bd
egilkh <egilkh@gmail.com>
2015-04-14 19:07:46 +0200

    Add SVG mime.

--- servish.js
+++ servish.js
@@ -36,6 +36,7 @@ var defaults = {
       '.css': 'text/css; charset=utf-8',
       '.html': 'text/html; charset=utf-8',
       '.json': 'application/json; charset=utf-8',
+      '.svg': 'image/svg+xml; charset=utf-8',
       '.png': 'image/png',
       '.jpg': 'image/jpeg',
       '.jpeg': 'image/jpeg',
